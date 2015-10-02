a27ef542537f82630668cb57dd3f2eb1283ad5ce
Michael Krumdick <mrkrumdick@gmail.com>
2015-10-02 19:46:45 -0400

    Initial Commit

---
+++ jamocracy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jamocracy
+//
+//  Created by Michael Krumdick on 10/2/15.
+//  Copyright © 2015 orbisbutbetter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
