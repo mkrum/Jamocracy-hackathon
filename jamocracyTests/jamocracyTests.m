8f111d28da6f1ba0c593f536c6e377e61b31935c
Michael Krumdick <mrkrumdick@gmail.com>
2015-10-02 22:07:10 -0400

    Initial Commit

---
+++ jamocracyTests/jamocracyTests.m
@@ -0,0 +1,39 @@
+//
+//  jamocracyTests.m
+//  jamocracyTests
+//
+//  Created by Michael Krumdick on 10/2/15.
+//  Copyright © 2015 orbisbutbetter. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface jamocracyTests : XCTestCase
+
+@end
+
+@implementation jamocracyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
